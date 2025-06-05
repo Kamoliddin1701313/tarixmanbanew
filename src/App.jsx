775f0edc1e216a9b74ae871d11a3ce24b74f4491
Kamoliddin1701313 <kamol170professor@gmail.com>
2025-06-05 10:17:52 +0500

    add qildim bugun

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,104 @@
+// import { Route, Routes, useNavigate } from "react-router-dom";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import Layouts from "./Layouts/Layouts";
+// import Home from "./Home/Home";
+// import About from "./Pages/About/About";
+// import Library from "./Pages/Library/Library";
+// import News from "./Pages/News/News";
+// import NotFound from "./Components/NotFound/NotFound";
+// import ScrollToTop from "./Components/ScrollToTop";
+// import axios from "axios";
+// import NewsDetail from "./Details/NewsDetail/NewsDetail";
+// import LibraryDetail from "./Details/LibraryDetail/LibraryDetail";
+// import HomeImageDetail from "./Details/HomeImageDetail/HomeImageDetail";
+// import ArchaeologicalItemDetail from "./Details/ArchaeologicalItemDetail/ArchaeologicalItemDetail";
+// // import ImageDetailView from "./Details/ImageDetailView/ImageDetailView";
+// import ImageViewDetail from "./Details/ImageViewDetail/ImageViewDetail";
+// import { createContext, useEffect, useState } from "react";
+// import Search from "./Components/Search/Search";
+// import Modal from "./Details/HomeImageDetail/Modal/Modal";
+// import Login from "./Login/Login";
+
+// axios.defaults.baseURL = "https://backend.tarixmanba.uz/api/";
+
+// export const ValueContext = createContext();
+
+// function App() {
+//   const [searchValue, setSearchValue] = useState("");
+//   const [openIcon, setOpenIcon] = useState(false);
+
+//   // Login qismining codi
+//   const navigate = useNavigate();
+//   const token = localStorage.getItem("parol");
+//   useEffect(() => {
+//     if (!token) {
+//       navigate("/login");
+//     } else if (token) {
+//       navigate("/");
+//     }
+//   }, []);
+
+//   const [checkedItemsChog, setCheckedItemsChog] = useState([]);
+//   const [checkedItems, setCheckedItems] = useState([]);
+//   const [searchText, setSearchText] = useState("");
+//   const [tempSearchValue, setTempSearchValue] = useState("");
+//   return (
+//     <ValueContext.Provider
+//       value={{
+//         searchValue,
+//         setSearchValue,
+
+//         openIcon,
+//         setOpenIcon,
+
+//         checkedItemsChog,
+//         setCheckedItemsChog,
+
+//         checkedItems,
+//         setCheckedItems,
+
+//         tempSearchValue,
+//         setTempSearchValue,
+
+//         searchText,
+//         setSearchText,
+//       }}
+//     >
+//       <div>
+//         <ScrollToTop />
+//         <Routes>
+//           <Route path="/login" element={<Login />} />
+//           <Route element={<Layouts />}>
+//             <Route path="/" index element={<Home />} />
+//             <Route path="/library" element={<Library />} />
+//             <Route path="/news" element={<News />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/search" element={<Search />} />
+//             <Route path="*" element={<NotFound />} />
+
+//             {/* DETAILS */}
+//             <Route path="/news/:id" element={<NewsDetail />} />
+//             <Route path="/library/:id" element={<LibraryDetail />} />
+//             <Route path="/homeImageDetail/:id" element={<HomeImageDetail />} />
+//             <Route path="/modal/:id" element={<Modal />} />
+//             <Route
+//               path="/homeImageDetail/:id/:detailId"
+//               element={<ImageViewDetail />}
+//             />
+//             <Route
+//               path="/archaeological/:id"
+//               element={<ArchaeologicalItemDetail />}
+//             />
+//           </Route>
+//         </Routes>
+//       </div>
+//     </ValueContext.Provider>
+//   );
+// }
+
+// export default App;
+
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,12 +114,15 @@ import NewsDetail from "./Details/NewsDetail/NewsDetail";
 import LibraryDetail from "./Details/LibraryDetail/LibraryDetail";
 import HomeImageDetail from "./Details/HomeImageDetail/HomeImageDetail";
 import ArchaeologicalItemDetail from "./Details/ArchaeologicalItemDetail/ArchaeologicalItemDetail";
-// import ImageDetailView from "./Details/ImageDetailView/ImageDetailView";
 import ImageViewDetail from "./Details/ImageViewDetail/ImageViewDetail";
 import { createContext, useEffect, useState } from "react";
 import Search from "./Components/Search/Search";
 import Modal from "./Details/HomeImageDetail/Modal/Modal";
 import Login from "./Login/Login";
+
+// Toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 axios.defaults.baseURL = "https://backend.tarixmanba.uz/api/";
 
@@ -28,9 +132,9 @@ function App() {
   const [searchValue, setSearchValue] = useState("");
   const [openIcon, setOpenIcon] = useState(false);
 
-  // Login qismining codi
   const navigate = useNavigate();
   const token = localStorage.getItem("parol");
+
   useEffect(() => {
     if (!token) {
       navigate("/login");
@@ -43,24 +147,20 @@ function App() {
   const [checkedItems, setCheckedItems] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [tempSearchValue, setTempSearchValue] = useState("");
+
   return (
     <ValueContext.Provider
       value={{
         searchValue,
         setSearchValue,
-
         openIcon,
         setOpenIcon,
-
         checkedItemsChog,
         setCheckedItemsChog,
-
         checkedItems,
         setCheckedItems,
-
         tempSearchValue,
         setTempSearchValue,
-
         searchText,
         setSearchText,
       }}
@@ -76,8 +176,6 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/search" element={<Search />} />
             <Route path="*" element={<NotFound />} />
-
-            {/* DETAILS */}
             <Route path="/news/:id" element={<NewsDetail />} />
             <Route path="/library/:id" element={<LibraryDetail />} />
             <Route path="/homeImageDetail/:id" element={<HomeImageDetail />} />
@@ -92,6 +190,17 @@ function App() {
             />
           </Route>
         </Routes>
+
+        {/* ToastContainer faqat bir marta */}
+        <ToastContainer
+          position="top-right"
+          autoClose={600}
+          hideProgressBar={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="light"
+        />
       </div>
     </ValueContext.Provider>
   );
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,8 +1,93 @@
+// import React, { useState } from "react";
+// import style from "./login.module.scss";
+// import { useNavigate } from "react-router-dom";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// function Login() {
+//   const [phone, setPhone] = useState("");
+//   const [parol, setParol] = useState("");
+//   const [loginParol, setLoginParol] = useState(true);
+//   const navigate = useNavigate();
+
+//   const HandeSubmit = (event) => {
+//     event.preventDefault();
+
+//     if (phone == "911111111" && parol == "12345678") {
+//       localStorage.setItem("parol", "parol");
+//       toast.success("Muvaffaqiyatli kirdingiz!");
+//       setTimeout(() => {
+//         navigate("/");
+//       }, 1000);
+//     } else {
+//       setLoginParol(false);
+//     }
+//   };
+
+//   return (
+//     <div className={style["login-container"]}>
+
+//       <ToastContainer
+//         position="top-right"
+//         autoClose={600}
+//         hideProgressBar={false}
+//         closeOnClick
+//         pauseOnHover
+//         draggable
+//         theme="light"
+//       />
+
+//       <form onSubmit={HandeSubmit} className={style.login}>
+//         <h1>Hisobingizga kiring!</h1>
+//         <div className={style.inputCard}>
+//           <label className={style.label} htmlFor="phone">
+//             Telefon raqam:
+//           </label>
+
+//           <input
+//             id="phone"
+//             value={phone}
+//             type="text"
+//             placeholder="Admin bo'lib kiring? Tel:"
+//             onChange={({ target: { value } }) => setPhone(value)}
+//             required
+//           />
+//           <span style={{ marginTop: "6px", color: "#ff0000db" }}>
+//             {!loginParol && "Telefon raqam mos kelmadi!"}
+//           </span>
+//         </div>
+
+//         <div className={style.inputCard}>
+//           <label className={style.label} htmlFor="phone">
+//             Parol:
+//           </label>
+//           <input
+//             value={parol}
+//             type="text"
+//             autoComplete="on"
+//             placeholder="Parolni kiriting"
+//             onChange={({ target: { value } }) => setParol(value)}
+//             required
+//           />
+//           <span style={{ marginTop: "6px", color: "#ff0000db" }}>
+//             {!loginParol && "Parol mos kelmadi!"}
+//           </span>
+//         </div>
+
+//         <button className={style.loginBtn} type="submit">
+//           Yuborish
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import style from "./login.module.scss";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify"; // faqat toast funksiyasini olib keldik
 
 function Login() {
   const [phone, setPhone] = useState("");
@@ -13,12 +98,12 @@ function Login() {
   const HandeSubmit = (event) => {
     event.preventDefault();
 
-    if (phone == "911111111" && parol == "12345678") {
+    if (phone === "911111111" && parol === "12345678") {
       localStorage.setItem("parol", "parol");
       toast.success("Muvaffaqiyatli kirdingiz!");
       setTimeout(() => {
         navigate("/");
-      }, 1500);
+      }, 1000);
     } else {
       setLoginParol(false);
     }
@@ -26,28 +111,19 @@ function Login() {
 
   return (
     <div className={style["login-container"]}>
-      <ToastContainer
-        position="top-right"
-        autoClose={1200}
-        hideProgressBar={false}
-        closeOnClick
-        pauseOnHover
-        draggable
-        theme="light"
-      />
       <form onSubmit={HandeSubmit} className={style.login}>
         <h1>Hisobingizga kiring!</h1>
+
         <div className={style.inputCard}>
           <label className={style.label} htmlFor="phone">
             Telefon raqam:
           </label>
-
           <input
             id="phone"
             value={phone}
             type="text"
             placeholder="Admin bo'lib kiring? Tel:"
-            onChange={({ target: { value } }) => setPhone(value)}
+            onChange={(e) => setPhone(e.target.value)}
             required
           />
           <span style={{ marginTop: "6px", color: "#ff0000db" }}>
@@ -56,15 +132,16 @@ function Login() {
         </div>
 
         <div className={style.inputCard}>
-          <label className={style.label} htmlFor="phone">
+          <label className={style.label} htmlFor="password">
             Parol:
           </label>
           <input
+            id="password"
             value={parol}
-            type="text"
+            type="password"
             autoComplete="on"
             placeholder="Parolni kiriting"
-            onChange={({ target: { value } }) => setParol(value)}
+            onChange={(e) => setParol(e.target.value)}
             required
           />
           <span style={{ marginTop: "6px", color: "#ff0000db" }}>
