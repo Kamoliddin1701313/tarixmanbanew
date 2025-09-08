36d9dc9c9c4ceca7bbb74f5d22d42bf8061c1877
Kamoliddin1701313 <kamol170professor@gmail.com>
2025-09-08 23:31:34 +0500

    git hubga push qildim yana tarixmanbani o'zgartirish kiritib

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./about.module.scss";
 import axios from "axios";
 import Loading from "../../Loading/Loading";
@@ -31,33 +31,7 @@ function About() {
         <Loading />
       ) : data.length ? (
         <div>
-          <div className={style.location}>
-            <img loading="lazy" decoding="async" src={logo} alt="logo" />
-            <h1>Biz haqimizda</h1>
-            <h1>O‘zbekiston Respublikasi Fanlar akademiyasi Tarix instituti</h1>
-            <p>
-              100060, Toshkent sh., Mirobod tumani Shahrisabz tor ko‘chasi, 5-uy
-            </p>
-            <p>Telefon: (+998 71) 233-54-70</p>
-            <p>Faks: (+998 71) 233-39-91</p>
-            <p>
-              Rasmiy sayt: <a href="https://fati.uz">fati.uz</a>
-            </p>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5992.963462466031!2d69.234307!3d41.320137!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDE5JzEyLjUiTiA2OcKwMTQnMDMuNSJF!5e0!3m2!1sen!2sus!4v1743963492601!5m2!1sen!2sus"
-              width="100%"
-              height="500"
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              style={{
-                border: "none",
-                margin: "30px 0 0",
-                borderRadius: "0 0 20px 20px",
-              }}
-            ></iframe>
-          </div>
-
-          <div className={style.lineX}></div>
+          {/* <div className={style.lineX}></div> */}
 
           <h1 className={style.users}>Ishtirokchilar</h1>
 
@@ -78,6 +52,37 @@ function About() {
                   </div>
                 </div>
               ))}
+          </div>
+
+          <div className={style.location_about}>
+            <div className={style.location}>
+              <img loading="lazy" decoding="async" src={logo} alt="logo" />
+              <h1>Biz haqimizda</h1>
+              <h1>
+                O‘zbekiston Respublikasi Fanlar akademiyasi Tarix instituti
+              </h1>
+              <p>
+                100060, Toshkent sh., Mirobod tumani Shahrisabz tor ko‘chasi,
+                5-uy
+              </p>
+              <p>Telefon: (+998 71) 233-54-70</p>
+              <p>Faks: (+998 71) 233-39-91</p>
+
+              <p>
+                Rasmiy sayt: <a href="https://fati.uz">fati.uz</a>
+              </p>
+            </div>
+
+            <div className={style.iframe}>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d5992.963462466031!2d69.234307!3d41.320137!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDE5JzEyLjUiTiA2OcKwMTQnMDMuNSJF!5e0!3m2!1sen!2sus!4v1743963492601!5m2!1sen!2sus"
+                width="100%"
+                height="100%"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                style={{ border: "none" }}
+              ></iframe>
+            </div>
           </div>
         </div>
       ) : (
